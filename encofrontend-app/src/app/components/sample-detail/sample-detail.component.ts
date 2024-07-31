85a84879db86a1d41b5aa58696c1f1b6e940eeb3
joooki82 <61735967+joooki82@users.noreply.github.com>
2024-07-31 10:02:33 +0200

    sampleDetail timepicker

--- encofrontend-app/src/app/components/sample-detail/sample-detail.component.ts
+++ encofrontend-app/src/app/components/sample-detail/sample-detail.component.ts
@@ -1,15 +1,15 @@
 import {Component, OnInit} from '@angular/core';
 import {SampleDetail} from "../../model/sample-detail/sample-detail.model";
 import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
+import {MatFormField, MatFormFieldModule} from "@angular/material/form-field";
+import { NgxMatDatetimePickerModule, NgxMatNativeDateModule, NgxMatTimepickerModule } from '@angular-material-components/datetime-picker';
+import { MatInputModule} from "@angular/material/input";
+import { MatButtonModule} from "@angular/material/button";
 import {SampleDetailService} from "../../service/sample-detail.service";
+import {CommandModule} from "@angular/cli/src/command-builder/command-module";
 import {CommonModule} from "@angular/common";
-import moment, {now} from 'moment';
-import { MatFormFieldModule} from "@angular/material/form-field";
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatNativeDateModule } from '@angular/material/core';
-import { MatInputModule } from '@angular/material/input';
-import { MatMomentDateModule } from '@angular/material-moment-adapter';
-import {NgxMatDatetimePickerModule} from "@angular-material-components/datetime-picker";
+
+
 
 
 @Component({
@@ -17,19 +17,15 @@ import {NgxMatDatetimePickerModule} from "@angular-material-components/datetime-
   standalone: true,
   imports: [
     CommonModule,
-    MatDatepickerModule,
-    MatNativeDateModule,
-    MatInputModule,
-    MatFormFieldModule,
-    MatMomentDateModule,
     ReactiveFormsModule,
-    FormsModule,
-    MatDatepickerModule,
-    MatNativeDateModule,
-    MatInputModule,
-    MatFormFieldModule,
-    MatMomentDateModule,
+    MatFormField,
     NgxMatDatetimePickerModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    NgxMatDatetimePickerModule,
+    NgxMatNativeDateModule,
+    NgxMatTimepickerModule
   ],
   templateUrl: './sample-detail.component.html',
   styleUrl: './sample-detail.component.css'
@@ -55,8 +51,8 @@ export class SampleDetailComponent implements OnInit {
       pressure: ['', Validators.required],
       sampleVolumeFlowRate: ['', Validators.required],
       adjustmentMethodId: ['', Validators.required],
-      startTime: [now(), Validators.required],
-      endTime: [now(), Validators.required],
+      startTime: ['', Validators.required],
+      endTime: ['', Validators.required],
       samplingTypeId: ['', Validators.required]
     });
   }
