e4b147712f5dccbb66623b75745595d17179e917
joooki82 <61735967+joooki82@users.noreply.github.com>
2024-07-31 15:04:30 +0200

    sampleDetail timepicker, and detailes display of parameters and several contaminants in sample details

--- encofrontend-app/src/app/components/sample-detail/sample-detail.component.ts
+++ encofrontend-app/src/app/components/sample-detail/sample-detail.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {SampleDetail} from "../../model/sample-detail/sample-detail.model";
-import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
+import {FormArray, FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
 import {MatFormField, MatFormFieldModule} from "@angular/material/form-field";
 import { NgxMatDatetimePickerModule, NgxMatNativeDateModule, NgxMatTimepickerModule } from '@angular-material-components/datetime-picker';
 import { MatInputModule} from "@angular/material/input";
@@ -8,6 +8,7 @@ import { MatButtonModule} from "@angular/material/button";
 import {SampleDetailService} from "../../service/sample-detail.service";
 import {CommandModule} from "@angular/cli/src/command-builder/command-module";
 import {CommonModule} from "@angular/common";
+import {MatIcon} from "@angular/material/icon";
 
 
 
@@ -25,7 +26,8 @@ import {CommonModule} from "@angular/common";
     MatButtonModule,
     NgxMatDatetimePickerModule,
     NgxMatNativeDateModule,
-    NgxMatTimepickerModule
+    NgxMatTimepickerModule,
+    MatIcon
   ],
   templateUrl: './sample-detail.component.html',
   styleUrl: './sample-detail.component.css'
@@ -44,7 +46,7 @@ export class SampleDetailComponent implements OnInit {
       topicNumber: ['', Validators.required],
       uniqueSampleNumber: ['', Validators.required],
       insideSamplingLocation: ['', Validators.required],
-      contaminantId: ['', Validators.required],
+      contaminantIds: this.fb.array([this.fb.control('', Validators.required)]), // Initialize with one control
       workerExamined: ['', Validators.required],
       temperature: ['', Validators.required],
       humidity: ['', Validators.required],
@@ -57,6 +59,18 @@ export class SampleDetailComponent implements OnInit {
     });
   }
 
+  get contaminantIds() {
+    return this.sampleDetailForm.get('contaminantIds') as FormArray;
+  }
+
+  addContaminant() {
+    this.contaminantIds.push(this.fb.control('', Validators.required));
+  }
+
+  removeContaminant(index: number) {
+    this.contaminantIds.removeAt(index);
+  }
+
   ngOnInit(): void {
     this.loadSampleDetails()
   }
@@ -64,6 +78,7 @@ export class SampleDetailComponent implements OnInit {
   loadSampleDetails() {
     this.sampleDetailService.getAllSampleDetails().subscribe(data => {
       this.sampleDetailList = data;
+      console.log(this.sampleDetailList)
     });
   }
 
--- encofrontend-app/src/app/model/sample-detail/sample-detail.model.ts
+++ encofrontend-app/src/app/model/sample-detail/sample-detail.model.ts
@@ -1,19 +1,37 @@
 import { Decimal } from 'decimal.js';
 
+export interface Contaminant {
+  id: number;
+  name: string;
+}
+
+export interface AdjustmentMethod {
+  id: number;
+  code: string;
+  description: string;
+}
+
+export interface SamplingType {
+  id: number;
+  code: string;
+  description: string;
+}
+
 export interface SampleDetail {
   detailId: number;
   seriesNumber: string;
   topicNumber: string;
   uniqueSampleNumber: string;
   insideSamplingLocation: string;
-  contaminantId: number;
+  contaminants: Contaminant[];
   workerExamined: string;
-  temperature: Decimal;
-  humidity: Decimal;
-  pressure: Decimal;
+  temperature: number;
+  humidity: number;
+  pressure: number;
   sampleVolumeFlowRate: string;
-  adjustmentMethodId: number;
+  adjustmentMethod: AdjustmentMethod;
   startTime: Date;
   endTime: Date;
-  samplingTypeId: number;
+  samplingType: SamplingType;
 }
+
