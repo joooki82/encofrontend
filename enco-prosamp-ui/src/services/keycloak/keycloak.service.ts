aa4bc2ba4cff0060802c1c71d549d14bda02cdb9
joooki82 <61735967+joooki82@users.noreply.github.com>
2024-12-01 21:19:35 +0100

    initializing project with Keycloak

--- enco-prosamp-ui/src/services/keycloak/keycloak.service.ts
+++ enco-prosamp-ui/src/services/keycloak/keycloak.service.ts
@@ -8,11 +8,29 @@ export class KeycloakService {
 
   private _keycloak: Keycloak | undefined;
 
+  get keycloak()  {
+    if (!this._keycloak) {
+      this._keycloak = new Keycloak({
+        url: 'http://localhost:8080/',
+        realm: 'Encotech',
+        clientId: 'enco-prosamp-angular-ui',
+      });
+    }
+    return this._keycloak;
+  }
+
   constructor() {}
 
   async init(): Promise<void> {
-    console.log('KeycloakService initializing...');
-    await new Promise((resolve) => setTimeout(resolve, 1000));
-    console.log('KeycloakService initialized.');
+    console.log('Authenticating user...');
+
+    const authenticated: boolean = await this.keycloak?.init({
+      onLoad: 'login-required',
+    });
+
+    if (authenticated) {
+      console.log('User authenticated.');
+    }
+
   }
 }
